6e965888d700facc2ed8de4654a84e1b4f1dbfad
Johan Russouw <johanrussouw490@gmail.com>
2026-08-22 18:45:06 +0200

    finished wiith sidebar and /admin page ui

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,66 @@
+import Link from "next/link";
 
 export default function DashboardPage() {
   return (
-    <div>DashboardPage</div>
+    <main className="md:ml-64 p-4 sm:p-6 min-h-screen">
+        {/* header */}
+        <div className="mb-6 sm:mb-8">
+            <h2 className="text-xl sm:text-2xl font-semibold text-text">
+                Dashboard
+            </h2>
+            <p className="text-sm text-gray-400 mt-1">
+                Welocome baqk, Here's what's happening with your blog.
+            </p>
+        </div>
+        {/* stats */}
+        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mb-8 sm:mb-10">
+            <div className="bg-card border border-border rounded-xl p-4">
+                <p className="text-sm text-gray-400">Posts</p>
+                <h2 className="text-xl sm:text-02xl font-semibold text-text mt-2">12</h2>
+            </div>
+             <div className="bg-card border border-border rounded-xl p-4">
+                <p className="text-sm text-gray-400">Authors</p>
+                <h2 className="text-xl sm:text-02xl font-semibold text-text mt-2">3</h2>
+            </div>
+        </div>
+        {/* Recent Posts */}
+        <div className="bg-card border border-border rounded-xl p-4 sm:p-5">
+         <div className="flex justify-between items-center mb-4">
+               <h2 className="text-base sm:text-lg font-semibold text-text">Recent Posts</h2>
+            <Link href="/admin/posts" className="text-sm text-primary hover:underline" >View all</Link>
+         </div>
+         <div className="space-y-4">
+            <div className="py-4 sm:py-6 flex items-center justify-between">
+               <div>
+
+                 <p className="text-sm text-text font-medium">
+                    Building a Fullstack Blog with Next.js
+                </p>
+                <p className="text-gray-400 text-xs">
+                    Technolog o 5 min read
+                </p>
+               </div>
+               <span className="text-xs text-gray-500">
+                Draft
+               </span>
+            </div>
+              <div className="py-4 sm:py-6 flex items-center justify-between">
+               <div>
+
+                 <p className="text-sm text-text font-medium">
+                    Building a Fullstack Blog with Next.js
+                </p>
+                <p className="text-gray-400 text-xs">
+                    Technolog o 5 min read
+                </p>
+               </div>
+               <span className="text-xs text-green-400">
+                Published
+               </span>
+            </div>
+              
+            </div>
+         </div>
+    </main>
   )
 }
--- src/app/components/admin/Sidebar.tsx
+++ src/app/components/admin/Sidebar.tsx
@@ -2,8 +2,10 @@
 
 import { usePathname } from "next/navigation"
 import { useState } from "react"
-import { FiFileText, FiHome, FiMenu, FiUsers } from "react-icons/fi"
+import { FiFileText, FiHome, FiMenu, FiUsers, FiX } from "react-icons/fi"
 import Logo from "../nabar/Logo"
+import Link from "next/link"
+import { BiLogOut } from "react-icons/bi"
 
 const links = [
     { name: "Dashboard", href: "/admin", icon: FiHome},
@@ -20,16 +22,35 @@ export default function Sidebar() {
     {/* top bar(mobile device) */}
     <div className="md:hidden flex iems-center justify-between p-4 border-b border-border bg-background">
         <Logo />
-        <button>
+        <button onClick={(() => setOpen(true))}>
             <FiMenu size={22} className="text-text z-100 " />
         </button>
     </div>
     {/* overlay */}
-    <div className="fixed inset-0 bg-black/50 z-40 md:hidden"/>
+    { open && <div className="fixed inset-0 bg-black/50 z-40 md:hidden" onClick={(() => setOpen(false))}/>}
     {/* sidebar */}
     <aside className={`fixed top-0 left-0 h-screen w-64 bg-card border-r border-border p-5 flex flex-col z-50 transform transition-transform duration-300 md:translate-x-0 ${open ? "translate-x-0" : "-translate-x-full"}`} >
         <div className="flex items-center justify-betweeb mb-8">
             <Logo />
+            <button onClick={(() => setOpen(false))} className="md:hidden ">
+                <FiX className="text-text" size={22} />
+            </button>
+        </div>
+        <nav className="flex flex-col gao-2">
+            {links.map((link) => { 
+                const isActive = pathname == link.href
+                const Icon = link.icon
+                return (<Link href={link.href} key={link.name} onClick={(()=> setOpen(false))} className={`flex items-center gap-3 px-4 py-6 rounded-lg text-lg transition ${isActive ? "bg-primary text-white" : "text-gray-400 hover:bg-surface hover:text-white"}`} >
+                    <Icon size={18}/>
+                    {link.name}
+                </Link>)}
+            )}
+        </nav>
+        <div className="mt-auto pt-6 border-t border-border">
+            <button className="w-full flex items-center gap-3 px-4 py-2 text-sm text-gray-400 hover:text-white transition">
+                <BiLogOut size={18}/>
+                Logout
+            </button>
         </div>
     </aside>
     </>
